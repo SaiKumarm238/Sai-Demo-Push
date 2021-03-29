aa2dd45f720e0a7f00f5ee9e96ec1d53c1f00a12
Sai Kumar Manikonda <sm21183@corp.ojas-it.com>
2021-03-29 14:09:27 +0530

    t commit

--- sai.py
+++ sai.py
@@ -1,2 +1,10 @@
 for i in range(1000):
     print("I am working in Ojas")
+
+
+
+class Employee(models.Model):
+   name = models.CharField(max_length=100)
+   adress = models.TextField()
+   email= models.EmailField(max_length=100)
+
